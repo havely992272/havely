a0b97bbbbe8e4818fc2d018c91fb9e0f28ce640e
havely992272 <havely992272@gmail.com>
2025-11-15 14:27:19 +0500

    Remove all resources and dependencies

--- app/src/main/java/com/shadow/messenger/MainActivity.java
+++ app/src/main/java/com/shadow/messenger/MainActivity.java
@@ -6,6 +6,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // NO setContentView - empty activity
     }
 }
